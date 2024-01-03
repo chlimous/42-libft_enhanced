de9fe1865ea05500f9d669a6f891ee76d30bc1a4
Charles <charles.limousin@gmail.com>
2024-01-03 01:47:30 +0100

    Added ft_rev_int_tab function

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/09/07 00:26:57 by chlimous	       #+#    #+#	      */
-/*   Updated: 2023/12/31 04:38:03 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/01/03 01:44:32 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include "conv.h"
 # include "check.h"
 # include "string.h"
+# include "tab.h"
 # include "get_next_line.h"
 # include "ft_printf.h"
 
