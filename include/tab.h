43ffe7bd7565122195f949f71da72a4d37f5a562
Charles <charles.limousin@gmail.com>
2024-01-04 01:17:06 +0100

    Added ft_rev_long_tab function

--- include/tab.h
+++ include/tab.h
@@ -6,13 +6,14 @@
 /*   By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 01:43:52 by chlimous          #+#    #+#             */
-/*   Updated: 2024/01/03 01:44:13 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/01/04 01:13:38 by chlimous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TAB_H
 # define TAB_H
 
-void	ft_rev_int_tab(int *tab, int size);
+void	ft_rev_int_tab(int *tab, unsigned int size);
+void	ft_rev_long_tab(long *tab, unsigned int size);
 
 #endif
