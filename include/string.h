9b1aa6a34f0c47277828375fd7072d6ed65c91a6
Charles <charles.limousin@gmail.com>
2024-01-04 18:40:19 +0100

    Added ft_strndup function / Reworked ft_split to take a charset as separator / In ft_split, switched all index variables to size_t to avoid potential overflows

--- include/string.h
+++ include/string.h
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/13 13:00:57 by chlimous	       #+#    #+#	      */
-/*   Updated: 2023/12/31 05:02:40 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:32:21 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdbool.h>
 
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *src);
@@ -26,11 +27,12 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char const *sep_charset);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strncpy(char *dest, char *src, size_t size);
 char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strndup(const char *src, size_t size);
 
 #endif
