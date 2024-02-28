05e7e8def674e1a85ced4364876d72024a464c9a
Charles Limousin <contact@charleslimousin.com>
2024-02-28 02:05:22 +0100

    Added ft_ascii_to_nb function

--- conv/ft_ascii_to_nb.c
+++ conv/ft_ascii_to_nb.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_ascii_to_nb.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 02:17:59 by chlimous          #+#    #+#             */
-/*   Updated: 2024/02/28 02:03:32 by chlimous         ###   ########.fr       */
+/*   Created: 2024/02/28 01:16:04 by chlimous          #+#    #+#             */
+/*   Updated: 2024/02/28 01:18:01 by chlimous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *nptr)
+int	ft_ascii_to_nb(char ascii)
 {
-	int	res;
-	int	sign;
-
-	res = 0;
-	sign = 1;
-	while (ft_isspace(*nptr))
-		nptr++;
-	if (*nptr == '-')
-	{
-		sign = -1;
-		nptr++;
-	}
-	else if (*nptr == '+')
-		nptr++;
-	while (ft_isdigit(*nptr))
-	{
-		res = res * 10 + ft_ascii_to_nb(*nptr);
-		nptr++;
-	}
-	return (res * sign);
+	return (ascii - 48);
 }
--- include/conv.h
+++ include/conv.h
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/13 12:51:52 by chlimous	       #+#    #+#	      */
-/*   Updated: 2023/12/13 12:53:52 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/02/28 01:33:46 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int		ft_atoi(const char *nptr);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_itoa(int n);
+int		ft_ascii_to_nb(char ascii);
 
 #endif
