d2b7d80ce8ebfad78424a11db97ba8b604cb98b3
Charles Limousin <contact@charleslimousin.com>
2024-09-15 05:15:25 +0200

    Refactoring

--- str/ft_strcmp.c
+++ str/ft_strcmp.c
@@ -6,18 +6,14 @@
 /*   By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 20:58:40 by chlimous          #+#    #+#             */
-/*   Updated: 2024/09/15 02:20:54 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/09/15 05:14:13 by chlimous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
+#include <stdint.h>
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 0;
-	while (s1[i] == s2[i] && s1[i])
-		++i;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (ft_strncmp(s1, s2, SIZE_MAX));
 }
--- str/ft_strndup.c
+++ str/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2024/01/04 17:58:46 by chlimous	       #+#    #+#	      */
-/*   Updated: 2024/09/15 04:58:13 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/09/15 05:09:48 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ char	*ft_strndup(const char *src, size_t size)
 	dest = malloc(sizeof(char) * (size + 1));
 	if (!dest)
 		return (NULL);
-	return ((char *)memcpy(dest, src, size));
+	return ((char *)ft_memcpy(dest, src, size));
 }
