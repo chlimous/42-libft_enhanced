8fe9b405439ed7235e3edb4f22dc4ec66333af89
Charles Limousin <contact@charleslimousin.com>
2024-09-15 05:08:17 +0200

    Fixed norm

--- str/ft_strdup.c
+++ str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/09/07 00:39:43 by chlimous	       #+#    #+#	      */
-/*   Updated: 2024/09/15 04:53:17 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/09/15 05:07:18 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 char	*ft_strdup(const char *src)
 {
-	return(ft_strndup(src, SIZE_MAX));
+	return (ft_strndup(src, SIZE_MAX));
 }
