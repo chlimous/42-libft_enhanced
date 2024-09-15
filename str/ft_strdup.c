569be44bfa7e89dc9c639a67f9f1a5eb424aac35
Charles Limousin <contact@charleslimousin.com>
2024-09-15 05:06:50 +0200

    Refactoring

--- str/ft_strdup.c
+++ str/ft_strdup.c
@@ -6,27 +6,14 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/09/07 00:39:43 by chlimous	       #+#    #+#	      */
-/*   Updated: 2024/09/15 02:22:58 by chlimous         ###   ########.fr       */
+/*   Updated: 2024/09/15 04:53:17 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
+#include <stdint.h>
 
 char	*ft_strdup(const char *src)
 {
-	char	*dest;
-	size_t	i;
-
-	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return(ft_strndup(src, SIZE_MAX));
 }
--- str/ft_strndup.c
+++ str/ft_strndup.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
+/*									      */
+/*							  :::	   ::::::::   */
 /*   ft_strndup.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/04 17:58:46 by chlimous          #+#    #+#             */
-/*   Updated: 2024/09/15 02:42:13 by chlimous         ###   ########.fr       */
-/*                                                                            */
+/*						      +:+ +:+	      +:+     */
+/*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
+/*						  +#+#+#+#+#+	+#+	      */
+/*   Created: 2024/01/04 17:58:46 by chlimous	       #+#    #+#	      */
+/*   Updated: 2024/09/15 04:58:13 by chlimous         ###   ########.fr       */
+/*									      */
 /* ************************************************************************** */
 
 #include "libft.h"
@@ -16,19 +16,11 @@
 char	*ft_strndup(const char *src, size_t size)
 {
 	char	*dest;
-	size_t	i;
 
 	if (ft_strlen(src) < size)
 		size = ft_strlen(src);
 	dest = malloc(sizeof(char) * (size + 1));
 	if (!dest)
 		return (NULL);
-	i = 0;
-	while (src[i] && i < size)
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return ((char *)memcpy(dest, src, size));
 }
