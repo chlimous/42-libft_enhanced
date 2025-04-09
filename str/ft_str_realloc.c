c5a0c9a060f784362a5a80b6d34b1bd4035cae73
Charles Limousin <contact@charleslimousin.com>
2025-04-09 10:03:29 +0200

    Refactoring

--- str/ft_str_realloc.c
+++ str/ft_str_realloc.c
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 01:24:39 by chlimous          #+#    #+#             */
-/*   Updated: 2025/04/09 01:24:40 by chlimous         ###   ########.fr       */
+/*   Updated: 2025/04/09 10:02:11 by chlimous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,8 @@ char	*ft_str_realloc(char *str, size_t size)
 	new = malloc(size);
 	if (!new)
 		return (NULL);
-	if (size <= ft_strlen(str) + 1)
-		cpy_size = size;
-	else
+	cpy_size = size;
+	if (ft_strlen(str) + 1 < size)
 		cpy_size = ft_strlen(str) + 1;
 	ft_memcpy(new, str, cpy_size);
 	free(str);
